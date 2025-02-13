7088a2e8a293f34b0fc96907624a2ebf4a359527
Phatdat01 <phatdatlht2001@gmail.com>
2025-02-13 08:14:27 +0700

    feat: add aty

--- shop/models/appointment.py
+++ shop/models/appointment.py
@@ -4,6 +4,7 @@ class ShopAppointment(models.Model):
     _name = "shop.appointment"
     _inherit = ["mail.thread"]
     _description = "User Appointment"
+    _rec_name_search = ["reference", "user_id"]
     _rec_name="user_id"
 
     reference = fields.Char(string="Reference", default="New")
@@ -16,6 +17,8 @@ class ShopAppointment(models.Model):
     appointment_line_ids = fields.One2many(
         "shop.appointment.line","appointment_id",string="Lines"
     )
+    total_qty = fields.Float(compute="_compute_total_qty", string="Total Quantity", store=True)
+    date_of_birth = fields.Date(string="DOB", related="user_id.dob", store=True)
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -24,6 +27,19 @@ class ShopAppointment(models.Model):
             if not vals.get('reference') or vals['reference']=="New":
                 vals['reference'] = self.env['ir.sequence'].next_by_code('shop.appointment')
         return super().create(vals_list)
+    
+    @api.depends("appointment_line_ids","appointment_line_ids.qty")
+    def _compute_total_qty(self):
+        for rec in self:
+            rec.total_qty = sum(rec.appointment_line_ids.mapped("qty"))
+            # rec.total_qty = 0
+            # for line in rec.appointment_line_ids:
+            #     total_qty = total_qty+line.qty
+            # rec.total_qty = total_qty
+    
+    def compute_display_name(self):
+        for rec in self: 
+            rec.display_name = f"[{rec.reference}] {rec.user_id.name}"
     
     def action_confirm(self):
         for rec in self:
@@ -46,5 +62,5 @@ class ShopAppointmentLines(models.Model):
     _description="User appointment Lines"
 
     appointment_id = fields.Many2one("shop.appointment", string="Appointment")
-    product_id = fields.Many2one("product.product", string="Product")
+    product_id = fields.Many2one("product.product", string="Product", required=True)
     qty = fields.Float(string="Quantity")
--- shop/models/shop.py
+++ shop/models/shop.py
@@ -9,13 +9,11 @@ class ShopManagement(models.Model):
     dob = fields.Date(string="DOB", tracking=True)
     gender = fields.Selection([("male","Male"),("female","Female")], string="Gender", tracking=True)
     
-    reference = fields.Char(string="Reference", default="New")
-    user_id = fields.Many2one("shop.user", string="User", tracking=True)
-    date_appointment = fields.Date(string="Date")
-    note = fields.Text(string="Note")
-    state = fields.Selection([("draft","Draft"),("confirmed","Confirmed"),
-        ("ongoing","Ongoging"),("done","Done"),("cancel","Cancel"),                        
-    ], default="draft", tracking=True)
+    # state = fields.Char(string="State")
+    # reference = fields.Char(string="State")
+    # user_id = fields.Char(string="State")
+    # date_appointment = fields.Char(string="State")
+    # note = fields.Char(string="State")
 
     tag_ids = fields.Many2many("shop.tag","shop_tag_rel","user_id","tag_id",string="Tags")
 
