a00b7d68c89d77ffefe4fa13a558c7d3bc6d7ddb
Phatdat01 <phatdatlht2001@gmail.com>
2025-02-20 11:58:35 +0700

    fix: handle conflict user vs appointment

--- shop/models/shop.py
+++ shop/models/shop.py
@@ -8,12 +8,6 @@ class ShopManagement(models.Model):
     name = fields.Char(string="Name", required=True, tracking=True)
     dob = fields.Date(string="DOB", tracking=True)
     gender = fields.Selection([("male","Male"),("female","Female")], string="Gender", tracking=True)
-    
-    # state = fields.Char(string="State")
-    # reference = fields.Char(string="State")
-    # user_id = fields.Char(string="State")
-    # date_appointment = fields.Char(string="State")
-    # note = fields.Char(string="State")
 
     tag_ids = fields.Many2many("shop.tag","shop_tag_rel","user_id","tag_id",string="Tags")
 
